395a0dbb299a23857d278b761598fd8af9ef984e
rnozila2197 <yjamshid2197@gmail.com>
2025-12-07 15:11:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+# 4
+# i = int(input("Chiroq rangi kiriting: "))
+#
+# q = "Qizil"
+# s = "Sariq"
+# y = "Yashil"
+#
+# if s == 0:
+#     print("Tayyorlaning!")
+# elif y == 0:
+#     print("Yuring!")
+# else:
+#     print("Bunday rang yoq!")
+
+# 5
+# i = int(input("Internet tezligi: "))
+# 
+# if i > 100:
+#     print("Sizda ajayib tezlik!")
+# elif 50 and 100:
+#     print("Tezlik yahshi, lekin yahshilash mumkin")
+# elif 10 and 50:
+#     print("Tezlik past!")
+# elif i < 10:
+#     print("Bu tezlik bilan internet ishlatib bolmaydi!")
+
+# 6
+# i = int(input("Imtixon balli: "))
+# 
+# if i >= 90:
+#     print("A'lo!")
+# elif 75 and 89:
+#     print("Yahshi!")
+# elif 50 and 74:
+#     print("Qoniqarli")
+# elif i > 50:
+#     print("Yiqildingiz!")
